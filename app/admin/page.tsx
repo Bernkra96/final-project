cab7f5dc9e8e1cd2b05eb909f700639ff5833c13
Mona Lisa Octocat <110023253+Bernkra96@users.noreply.github.com>
2024-03-19 12:59:26 +0100

    New Admin page shows num of User

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,14 @@
 import Link from 'next/link';
 import { isAdmin } from '../../database/admins';
-import { getusers } from '../../database/users';
+import {
+  getnumberOfUsers,
+  getnumerofPostsbysingleUser,
+  getusers,
+} from '../../database/users';
 
 export default async function AdminPage() {
   const users = await getusers();
+  const nubberofUsers = await getnumberOfUsers();
 
   return (
     <section className=" mx-auto  max-w-7xl items-center p-6 lg:px-8  bg-gray-100 ">
@@ -11,6 +16,16 @@ export default async function AdminPage() {
         {' '}
         Hallo Admin{' '}
       </h1>
+      <h2 className="mx-auto justify-center p-1  flex font-semibold text-gray-900 ">
+        {' '}
+        There are {nubberofUsers?.count} users{' '}
+      </h2>
+
+      <h2 className="mx-auto justify-center p-1  flex font-semibold text-gray-900 ">
+        {' '}
+        Here are the list of users{' '}
+      </h2>
+
       <ul className=" justify-center items items-center ">
         {users.map(async (user) => (
           <li
@@ -25,9 +40,14 @@ export default async function AdminPage() {
                 className="mx-auto justify-center p-6 lg:px-4 "
                 href={`/profile/${user.username}`}
               >
-                <p className="mx-auto justify-center p-1  text-green-700   ">
+                <p className="mx-auto justify-center   text-green-700   ">
                   {' '}
                   {user.username}{' '}
+                </p>
+
+                <p className="mx-auto justify-center   text-green-700   ">
+                  {' '}
+                  User ID {user.id}{' '}
                 </p>
 
                 {(await isAdmin(user.id)) ? (
