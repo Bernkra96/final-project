31a54916fef86f9f1b8a63719f35a8ff9917e560
Mona Lisa Octocat <110023253+Bernkra96@users.noreply.github.com>
2024-03-23 09:31:07 +0100

    Make Login/Register Buttons not work if not min Inpuut

--- app/(auth)/register/registerFrom.tsx
+++ app/(auth)/register/registerFrom.tsx
@@ -54,10 +54,15 @@ export default function RegisterFrom() {
           placeholder="Password"
           className="mx-auto flex justify-center p-3 border-2 m-1 border-gray-300 rounded-md"
         />
-
-        <button className="flex w-full justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600">
-          Register
-        </button>
+        {username.length > 2 && password.length > 5 ? (
+          <button className="flex w-full justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600">
+            Register
+          </button>
+        ) : (
+          <p className=" w-full justify-center rounded-md  bg-gray-600  m-3 px-3 py-1.5  font-semibold leading-6 text-white shadow-sm hover:bg-orange-500 focus-visible:outline text-center focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600">
+            Register
+          </p>
+        )}
       </form>
       {error.length > 0 ? (
         <p className=" w-full justify-center rounded-md bg-red-600  m-3 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-smfocus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2">
