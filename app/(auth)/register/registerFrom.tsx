ad2c5008abbee5e762f37be4419fc8f107988409
Mona Lisa Octocat <110023253+Bernkra96@users.noreply.github.com>
2024-03-24 20:07:03 +0100

    Add user name Valitaion no Space

--- app/(auth)/register/registerFrom.tsx
+++ app/(auth)/register/registerFrom.tsx
@@ -8,7 +8,7 @@ export default function RegisterFrom() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-
+  const usernameValidatonRegex = /^^\S*$/;
   const router = useRouter();
 
   async function handelregister(event: React.FormEvent<HTMLFormElement>) {
@@ -54,12 +54,14 @@ export default function RegisterFrom() {
           placeholder="Password"
           className="mx-auto flex justify-center p-3 border-2 m-1 border-gray-300 rounded-md"
         />
-        {username.length > 2 && password.length > 5 ? (
+        {username.length > 2 &&
+        password.length > 5 &&
+        usernameValidatonRegex.test(username) ? (
           <button className="flex w-full justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600">
             Register
           </button>
         ) : (
-          <p className=" w-full justify-center rounded-md  bg-gray-600  m-3 px-3 py-1.5  font-semibold leading-6 text-white shadow-sm hover:bg-orange-500 focus-visible:outline text-center focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600">
+          <p className="w-full justify-center rounded-md bg-gray-600 m-3 px-3 py-1.5 font-semibold leading-6 text-white shadow-sm hover:bg-orange-500 focus-visible:outline text-center focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600">
             Register
           </p>
         )}
